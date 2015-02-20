8faab402cd9756e1282216b48a11d45f7991281a
Hans Rødtang <hansrodtang@gmail.com>
2015-02-20 14:59:06 +0100

    Added storage manager.

---
+++ core/storage.go
@@ -0,0 +1,81 @@
+package core
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"os"
+
+	"github.com/spf13/viper"
+)
+
+type storage struct {
+	Plugins map[string]*json.RawMessage `json:"plugins"`
+}
+
+var store storage
+
+func init() {
+	// TODO: Better reading
+	err := Read()
+	if err != nil {
+		p := make(map[string]*json.RawMessage)
+		store = storage{Plugins: p}
+	}
+}
+
+// Add marshals and stores plugin data
+func Add(plugin string, content interface{}) error {
+
+	b, err := json.Marshal(content)
+	if err != nil {
+		return err
+	}
+	store.Plugins[plugin] = (*json.RawMessage)(&b)
+	return nil
+}
+
+// Get fetches and unmarshals plugin data
+func Get(plugin string, content interface{}) error {
+	data, ok := store.Plugins[plugin]
+	if !ok {
+		return PluginNotFoundError{plugin + " not found"}
+	}
+	parseErr := json.Unmarshal(([]byte)(*data), &content)
+
+	if parseErr != nil {
+		return parseErr
+	}
+	return nil
+}
+
+// Read fetches plugin data from file
+func Read() error {
+	dat, readErr := ioutil.ReadFile(viper.GetString("Storage"))
+	if readErr != nil {
+		return readErr
+	}
+	parseErr := json.Unmarshal(dat, &store)
+	if parseErr != nil {
+		return parseErr
+	}
+	return nil
+}
+
+// Save stores plugin data to file
+func Save() error {
+
+	b, err := json.MarshalIndent(store, " ", "	")
+	if err != nil {
+		return err
+	}
+
+	f, err := os.Create(viper.GetString("Storage"))
+	if err != nil {
+		return err
+	}
+
+	defer f.Close()
+	f.WriteString(string(b))
+
+	return nil
+}
