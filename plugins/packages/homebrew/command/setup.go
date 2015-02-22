69cc4f2f9cc314ebfa5ec85dc356a1f48bc02d7e
Hans Rødtang <hansrodtang@gmail.com>
2015-02-22 07:10:23 +0100

    Added Homebrew setup.

--- plugins/packages/homebrew/command/setup.go
+++ plugins/packages/homebrew/command/setup.go
@@ -1,4 +1,16 @@
 package command
 
+import "github.com/hansrodtang/runcom/core"
+
 const brewCommand = "brew"
 const caskCommand = "brew-cask"
+
+func Setup() bool {
+	if !core.IsInstalled(brewCommand) {
+		install("ruby", "-e", []string{"$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"})
+	}
+	if !core.IsInstalled(caskCommand) {
+		Install("caskroom/cask/brew-cask")
+	}
+	return true
+}
