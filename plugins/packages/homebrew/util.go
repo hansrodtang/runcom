1fd5a41dd2795595250ebc11e90c0cb41b6781f0
Hans Rødtang <hansrodtang@gmail.com>
2015-02-22 07:15:43 +0100

    Removed old Homebrew installer.

--- plugins/packages/homebrew/util.go
+++ plugins/packages/homebrew/util.go
@@ -2,51 +2,8 @@
 
 package homebrew
 
-import (
-	"os"
-	"os/exec"
-
-	"github.com/hansrodtang/runcom/core"
-)
-
 type plugin struct {
 	Casks    []string `json:"casks"`
 	Formulas []string `json:"formulas"`
 	Taps     []string `json:"taps"`
-}
-
-func installBrew() bool {
-	installCmd := exec.Command("ruby", "-e", "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)")
-	installCmd.Stdout = os.Stdout
-	installCmd.Stderr = os.Stderr
-	installCmd.Stdin = os.Stdin
-
-	if err := installCmd.Start(); err != nil {
-		print(err)
-		return false
-	}
-	installCmd.Wait()
-	return true
-
-}
-
-func installCask() bool {
-	if !core.IsInstalled(BrewCmd) {
-		if !installBrew() {
-			return false
-		}
-	}
-
-	installCmd := exec.Command(BrewCmd, "install", "caskroom/cask/brew-cask")
-	installCmd.Stdout = os.Stdout
-	installCmd.Stderr = os.Stderr
-	installCmd.Stdin = os.Stdin
-
-	if err := installCmd.Start(); err != nil {
-		print("An error occured: ", err)
-		return false
-	}
-	installCmd.Wait()
-	return true
-
 }
