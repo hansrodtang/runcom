f2091a9135fa26297a7b81a31a3382bacb7869e9
Hans Rødtang <hansrodtang@gmail.com>
2015-02-22 07:10:01 +0100

    Refactored outdated command.

--- plugins/packages/homebrew/command/outdated.go
+++ plugins/packages/homebrew/command/outdated.go
@@ -1,24 +1,6 @@
 package command
 
-import (
-	"fmt"
-	"os/exec"
-	"regexp"
-)
-
 func Outdated() []string {
-
-	re := regexp.MustCompile(`^(d.*) \(\d.*\)$`)
-
-	cmd := exec.Command(brewCommand, "outdated")
-	output, err := cmd.Output()
-	if err != nil {
-		print(err)
-	}
-	s := re.FindSubmatch(output)
-	for _, v := range s {
-		fmt.Println(v)
-	}
-	return nil
-
+	s, _ := list(brewCommand, "outdated")
+	return s
 }
