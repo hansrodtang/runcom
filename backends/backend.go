7ecf5398c5751cbdc27a8e212457330be6dd35df
Hans Rødtang <hansrodtang@gmail.com>
2015-02-22 07:02:18 +0100

    Added backend manager.

--- backends/backend.go
+++ backends/backend.go
@@ -1,1 +1,34 @@
 package backend
+
+import "errors"
+
+func init() {
+	backends = make(map[string]Backend)
+}
+
+type ReadFunc func()
+type SaveFunc func()
+
+type Backend struct {
+	Name string
+	Read ReadFunc
+	Save SaveFunc
+}
+
+var backends map[string]Backend
+
+func Register(name string, read ReadFunc, save SaveFunc) {
+	backends[name] = Backend{name, read, save}
+}
+
+func Get(name string) (Backend, error) {
+	b, ok := backends[name]
+	if !ok {
+		return Backend{}, errors.New("no backend found")
+	}
+	return b, nil
+}
+
+func GetAll() map[string]Backend {
+	return backends
+}
